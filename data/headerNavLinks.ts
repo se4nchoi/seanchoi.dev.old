d5f776a9042a8dff6c931cf81bed2bfcd778cc5b
Sean Choi <yehyun.choi94@gmail.com>
2024-01-03 19:45:54 -0500

    initial setup for new blog

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/about', title: 'About' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+]
+
+export default headerNavLinks
